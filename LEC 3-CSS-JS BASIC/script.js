6c92cda904b34d64a15713d194ba9b4908afd4f2
hansrajathole <hansrajathole1@gmail.com>
2025-07-11 17:22:13 +0530

    CSS - background-image, overflow, transition, hover, display : block / none , js basic / datatype, array , function

---
+++ LEC 3-CSS-JS BASIC/script.js
@@ -0,0 +1,53 @@
+let a 
+
+// var a = 20
+// console.log(a);
+// a = 10
+// let a = 20
+// console.log(typeof a);
+// let str = "hansraj hui hiui"
+// console.log(typeof str);
+
+// let istrue = null
+// console.log(typeof istrue);
+
+// function declaration
+// fum()
+// function fum() {
+//     console.log("chal gya");
+    
+// }
+
+
+// function expresion
+// sum()
+// const sum = function() {
+//     console.log("second wala bhi chal gya");
+    
+// } 
+
+
+
+
+// a = 10
+
+
+// ARROW FUNCTION 
+// sum()
+// var sum = ( a, b)=>{
+//     return a+b
+    
+// }
+// let ans = sum(10 , 20)
+// console.log(sum(10,20));
+
+// ARRAY 
+let arr = [10,20,30,40]
+// console.log("YE hamara old arr hai :",arr);
+console.log("phle :",arr);
+
+let newArr = arr
+newArr.push(50)
+console.log("new wala",newArr);
+
+console.log("old wala",arr);
