efbea3625b395e54204b590a4a87c14a13e1d2c3
hansrajathole <hansrajathole1@gmail.com>
2025-07-17 08:19:58 +0530

    promise and async-await/ DOM

---
+++ LEC 5 JS AND DOM/app.js
@@ -0,0 +1,51 @@
+// function promise() {
+//     return new Promise((resolve , reject)=>{
+//     let num = Math.floor(Math.random()*2)
+
+//     if(num === 0){
+//         setTimeout(()=>{
+//             resolve("homework done")
+//         },2000)
+//     }else{
+//        setTimeout(()=>{
+//            reject("homework not done")
+//         },2000)
+//     }
+// })
+// }
+
+// console.log(promise);
+
+
+// promise.then((res)=>{
+//     console.log(res);
+//     console.log(promise);
+    
+    
+// })
+
+// .catch((err)=>{
+//     console.log(err);
+//     console.log(promise);
+    
+// })
+// async function sum() {
+//     try {
+        // let a = 10
+        // let b = 20
+        // console.log(a+b);
+//         let result = await promise()
+//         console.log(result);
+//         console.log("hello");
+        
+        
+        
+//     } catch (error) {
+//         // console.log(error);
+//         console.error(error)
+//     }
+// }
+
+// sum()
+
+// console.log(10/parseInt("hello"));
---
+++ LEC 5 JS AND DOM/script.js
@@ -0,0 +1,42 @@
+// let box = document.querySelectorAll(".box")
+
+// let box = document.querySelector("#box")
+// let box = document.getElementById("box")
+
+// let para = document.querySelector("p")
+
+// console.log(para.innerText);
+// console.log(para.textContent);
+// console.log(para.innerHTML);
+// para.innerHTML = "hello bhaiyo <b>hello</b>"
+// para.style.backgroundColor = "red"
+// para.style.color = "white"
+
+// para.classList.remove("para")
+// para.classList.add("green")
+// console.log(para.classList.add());
+
+// console.log();
+
+// let body = document.querySelector("body");
+// let btn = document.querySelector("button");
+
+// console.log(img);
+
+// btn.addEventListener("click", () => {
+//   // body.classList.toggle("dark")
+//   let img = document.createElement("img");
+//   img.setAttribute(
+//     "src",
+//     "https://image.pngaaa.com/816/3516816-middle.png"
+//   );
+
+//   body.appendChild(img);
+// });
+
+
+// btn.addEventListener("click", ()=>{
+//     let inp = document.querySelector("input")
+//     console.log(inp.value);
+    
+// })
