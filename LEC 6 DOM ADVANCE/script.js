b075efe458c4a4a59f0a8156bcbf1d218edc17c7
hansrajathole <hansrajathole1@gmail.com>
2025-07-18 14:22:03 +0530

    DOM ADVANCE TOPIC

---
+++ LEC 6 DOM ADVANCE/script.js
@@ -0,0 +1,72 @@
+// const btn = document.querySelector("button")
+
+// btn.addEventListener("click", ()=>{
+//     let parent = btn.parentElement
+//     console.log(parent);
+
+//     parent.style.height = "100px"
+//     parent.style.width = "100px"
+//     parent.style.backgroundColor = "green"
+    
+// })
+
+// const list = document.querySelector(".list")
+
+// list.addEventListener("click", ()=>{
+    // console.log(list.children);
+
+    // console.log(list.firstElementChild);
+    // console.log(list.lastElementChild);
+    // console.log(list.firstChild);
+    // console.log(list.lastChild);
+    // list.removeAttribute("class")
+
+    // console.log(list.id);
+    // list.id = "list1"
+    
+    // console.log(list.id);
+    
+    // console.log(list.nextElementSibling);
+    // console.log(list.previousElementSibling);
+    
+    
+    
+    
+// })
+
+
+let box = document.querySelector(".box")
+
+// box.addEventListener("mouseover",()=>{
+//     console.log("mouse aaya");
+    
+// })
+// box.addEventListener("mouseout",()=>{
+//     console.log("mouse gaya");
+    
+// })
+
+
+// box.addEventListener("keypress",()=>{
+//     console.log("key press");
+    
+// })
+
+
+let inp = document.querySelector("input")
+
+
+inp.addEventListener("keydown",(event)=>{
+    console.log(event);
+    
+    // console.log("key down");
+    
+})
+// inp.addEventListener("keypress",()=>{
+//     console.log("key presss");
+    
+// })
+// inp.addEventListener("keyup",()=>{
+//     console.log("key up");
+    
+// })
